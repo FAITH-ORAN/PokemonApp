1c58f9e5de9f6c185a9c465c7ed63b4f9245be16
Faiza <berrichif0717@gmail.com>
2022-06-14 10:47:15 +0200

    add liste of generations

--- src/app/composants/generations/generations.component.ts
+++ src/app/composants/generations/generations.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generations.component.css']
 })
 export class GenerationsComponent implements OnInit {
-
+  generation=["Génération 1","Génération 2","Génération 3","Génération 4","Génération 5","Génération 6","Génération 7",]
   constructor() { }
 
   ngOnInit(): void {
