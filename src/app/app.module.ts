1f9bad19defe032cb6172dadca1daf5ffadb559c
Faiza <berrichif0717@gmail.com>
2022-06-14 11:54:57 +0200

    add fr local and images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GenerationsComponent } from './composants/generations/generations.component';
 import { PokemonsComponent } from './composants/pokemons/pokemons.component';
+import localeFR from '@angular/common/locales/fr';
 
+
+registerLocaleData(localeFR); 
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +18,7 @@ import { PokemonsComponent } from './composants/pokemons/pokemons.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'fr'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
